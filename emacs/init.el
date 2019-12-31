562adcdf2390b62168a7a242cf43ebc3d024767b
Francesco Bertani <franz.bertani@gmail.com>
2019-12-31 15:20:57 +0100

    sensible defaults

--- emacs/init.el
+++ emacs/init.el
@@ -1,16 +1,13 @@
 (require 'package)
 (setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
 
 ;;; Don't litter my init file -- thanks aaronbieber
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
-
-;;; Standard package repositories
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("elpy" . "https://jorgenschaefer.github.io/packages/"))
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
